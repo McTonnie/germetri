23cd7e9d412a616f4f622eb4a5363e4b9aebac38
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-02-22 13:09:19 +0100

    Add project files.

---
+++ germetri/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace germetri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square kvadrat = new Square(10);
+            Square kvadrat2 = new Square(17);
+            Square kvadrat3 = new Square(27);
+            Square kvadrat4 = new Square(239);
+            Console.WriteLine(kvadrat.omkreds());
+            Console.WriteLine(kvadrat2.omkreds());
+            Console.WriteLine(kvadrat3.omkreds());
+            Console.WriteLine(kvadrat4.omkreds());
+
+
+            kvadrat.setside(100);
+            Console.WriteLine(kvadrat.omkreds());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ germetri/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace germetri
+{
+    public class Square
+    {
+
+        private double Side { get; set; }
+
+        public Square(double længde)
+        {
+            Side = længde;
+        }
+
+        public void setside(double længde)
+        {
+            Side = længde;
+        }
+        public double omkreds()
+        {
+            return Side * 4;
+            
+        }
+        public double areal()
+        {
+            return Math.Pow(Side, 2);
+
+        }
+
+
+
+
+
+    }
+}
